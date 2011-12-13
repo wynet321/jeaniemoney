1260cac6efe3e3e3ec36d4a9db36b5f87c851026
wynet321@gmail.com <wynet321@gmail.com@5251cf08-2463-f38c-1da6-1ed73504ea8b>
2011-12-13 05:20:52 +0000

    db utility draft version.

--- JeanieMoney/Utility/Database.cs
+++ JeanieMoney/Utility/Database.cs
@@ -1,35 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Data.SqlClient;
 using System.Data;
-using System.Data.OleDb;
 using System.Windows.Forms;
+using System.Data.Common;
 
 namespace JeanieMoney.Utility
 {
     class Database
     {
-        OleDbConnection connection;
-        private OleDbConnection getConnection()
+        private static DbProviderFactory dbProviderFactory;
+        private static DbConnection connection;
+
+        private static DbConnection getConnection()
         {
             if (null == connection)
-                connection = new OleDbConnection("Data Source=localhost\\sqlexpress;Initial Catalog=JeanieMoney;Integrated Security=True");
+            {
+                //Todo: need add to config file.
+                dbProviderFactory = DbProviderFactories.GetFactory(ProviderName.getPIN("SQLSERVER"));
+                connection = dbProviderFactory.CreateConnection();
+                connection.ConnectionString = "Data Source=localhost\\sqlexpress;Initial Catalog=JeanieMoney;Integrated Security=True";
+                connection.Open();
+            }
             return connection;
         }
 
-        public bool execTranx(string command)
+        public static int execTranx(string[] commands)
         {
-            OleDbConnection localconnection = getConnection();
-            OleDbCommand localcommand = new OleDbCommand();
-            OleDbTransaction tranx = connection.BeginTransaction();
+            DbConnection localconnection = getConnection();
+            DbCommand localcommand = connection.CreateCommand();
+            DbTransaction tranx = connection.BeginTransaction();
+            int affectedRows = 0;
             try
             {
-                localconnection.Open();
-                tranx.Begin();
-                localcommand.CommandText = command;
-                localcommand.ExecuteNonQuery();
+                foreach (string command in commands)
+                {
+                    localcommand.CommandText = command;
+                    affectedRows += localcommand.ExecuteNonQuery();
+                }
                 tranx.Commit();
             }
             catch (Exception e)
@@ -48,16 +54,38 @@ namespace JeanieMoney.Utility
             {
                 localconnection.Close();
             }
-
-            
-            return true;
+            return affectedRows;
         }
 
-        public DataSet execQuery(string command)
+        public static int execCommand(string command)
         {
-            DataSet ds = new DataSet();
-
-            return ds;
+            DbConnection localconnection = getConnection();
+            DbCommand localcommand = connection.CreateCommand();
+            int affectedRows = 0;
+            localcommand.CommandText = command;
+            affectedRows = localcommand.ExecuteNonQuery();
+            localconnection.Close();
+            return affectedRows;
         }
+        public static DataTable execQuery(string command)
+        {
+            DbConnection localConnection = getConnection();
+            DbCommand localCommand = connection.CreateCommand();
+            localCommand.CommandText = command;
+            DbDataAdapter dbDataAdapter = dbProviderFactory.CreateDataAdapter();
+            dbDataAdapter.SelectCommand = localCommand;
+            DataTable dataTable = new DataTable();
+            dbDataAdapter.Fill(dataTable);
+            return dataTable;
+        }
+
+        public static object execQuery(string command)
+        {
+            DbConnection localConnection = getConnection();
+            DbCommand localCommand = connection.CreateCommand();
+            localCommand.CommandText = command;
+            return localCommand.ExecuteScalar();
+        }
+
     }
 }
