d921f5e35885750771ce8434441a0e5e1ec116cb
wynet321@gmail.com <wynet321@gmail.com@5251cf08-2463-f38c-1da6-1ed73504ea8b>
2012-05-15 03:58:57 +0000

    new CategoryConfig with treeview.

---
+++ trunk/JeanieMoney/Forms/Config/CategoryConfig.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace JeanieMoney.Forms.Config
+{
+    partial class CategoryConfig
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeViewCategory = new System.Windows.Forms.TreeView();
+            this.contextMenuStripCategory = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItemNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemModify = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBoxCategory = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBoxInOrOut = new System.Windows.Forms.GroupBox();
+            this.radioButtonIncome = new System.Windows.Forms.RadioButton();
+            this.radioButtonOutcome = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.contextMenuStripCategory.SuspendLayout();
+            this.groupBoxCategory.SuspendLayout();
+            this.groupBoxInOrOut.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeViewCategory
+            // 
+            this.treeViewCategory.AllowDrop = true;
+            this.treeViewCategory.Location = new System.Drawing.Point(12, 12);
+            this.treeViewCategory.Name = "treeViewCategory";
+            this.treeViewCategory.ShowNodeToolTips = true;
+            this.treeViewCategory.Size = new System.Drawing.Size(366, 407);
+            this.treeViewCategory.TabIndex = 0;
+            this.treeViewCategory.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.treeViewCategory_ItemDrag);
+            this.treeViewCategory.DragDrop += new System.Windows.Forms.DragEventHandler(this.treeViewCategory_DragDrop);
+            this.treeViewCategory.DragEnter += new System.Windows.Forms.DragEventHandler(this.treeViewCategory_DragEnter);
+            this.treeViewCategory.MouseDown += new System.Windows.Forms.MouseEventHandler(this.treeViewCategory_MouseDown);
+            // 
+            // contextMenuStripCategory
+            // 
+            this.contextMenuStripCategory.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemNew,
+            this.toolStripMenuItemModify,
+            this.toolStripMenuItemDelete});
+            this.contextMenuStripCategory.Name = "contextMenuStripCategory";
+            this.contextMenuStripCategory.Size = new System.Drawing.Size(113, 70);
+            // 
+            // toolStripMenuItemNew
+            // 
+            this.toolStripMenuItemNew.Name = "toolStripMenuItemNew";
+            this.toolStripMenuItemNew.Size = new System.Drawing.Size(112, 22);
+            this.toolStripMenuItemNew.Text = "&New";
+            this.toolStripMenuItemNew.Click += new System.EventHandler(this.toolStripMenuItemNew_Click);
+            // 
+            // toolStripMenuItemModify
+            // 
+            this.toolStripMenuItemModify.Name = "toolStripMenuItemModify";
+            this.toolStripMenuItemModify.Size = new System.Drawing.Size(112, 22);
+            this.toolStripMenuItemModify.Text = "&Modify";
+            // 
+            // toolStripMenuItemDelete
+            // 
+            this.toolStripMenuItemDelete.Name = "toolStripMenuItemDelete";
+            this.toolStripMenuItemDelete.Size = new System.Drawing.Size(112, 22);
+            this.toolStripMenuItemDelete.Text = "&Delete";
+            // 
+            // groupBoxCategory
+            // 
+            this.groupBoxCategory.Controls.Add(this.label3);
+            this.groupBoxCategory.Controls.Add(this.textBox3);
+            this.groupBoxCategory.Controls.Add(this.groupBoxInOrOut);
+            this.groupBoxCategory.Controls.Add(this.button2);
+            this.groupBoxCategory.Controls.Add(this.label2);
+            this.groupBoxCategory.Controls.Add(this.textBox2);
+            this.groupBoxCategory.Controls.Add(this.label1);
+            this.groupBoxCategory.Controls.Add(this.textBox1);
+            this.groupBoxCategory.Controls.Add(this.button1);
+            this.groupBoxCategory.Location = new System.Drawing.Point(385, 13);
+            this.groupBoxCategory.Name = "groupBoxCategory";
+            this.groupBoxCategory.Size = new System.Drawing.Size(230, 316);
+            this.groupBoxCategory.TabIndex = 2;
+            this.groupBoxCategory.TabStop = false;
+            this.groupBoxCategory.Text = "groupBox1";
+            this.groupBoxCategory.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(63, 156);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxInOrOut
+            // 
+            this.groupBoxInOrOut.Controls.Add(this.radioButtonOutcome);
+            this.groupBoxInOrOut.Controls.Add(this.radioButtonIncome);
+            this.groupBoxInOrOut.Location = new System.Drawing.Point(25, 20);
+            this.groupBoxInOrOut.Name = "groupBoxInOrOut";
+            this.groupBoxInOrOut.Size = new System.Drawing.Size(179, 73);
+            this.groupBoxInOrOut.TabIndex = 6;
+            this.groupBoxInOrOut.TabStop = false;
+            // 
+            // radioButtonIncome
+            // 
+            this.radioButtonIncome.AutoSize = true;
+            this.radioButtonIncome.Location = new System.Drawing.Point(6, 30);
+            this.radioButtonIncome.Name = "radioButtonIncome";
+            this.radioButtonIncome.Size = new System.Drawing.Size(60, 17);
+            this.radioButtonIncome.TabIndex = 0;
+            this.radioButtonIncome.TabStop = true;
+            this.radioButtonIncome.Text = "Income";
+            this.radioButtonIncome.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonOutcome
+            // 
+            this.radioButtonOutcome.AutoSize = true;
+            this.radioButtonOutcome.Location = new System.Drawing.Point(84, 30);
+            this.radioButtonOutcome.Name = "radioButtonOutcome";
+            this.radioButtonOutcome.Size = new System.Drawing.Size(68, 17);
+            this.radioButtonOutcome.TabIndex = 1;
+            this.radioButtonOutcome.TabStop = true;
+            this.radioButtonOutcome.Text = "Outcome";
+            this.radioButtonOutcome.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(63, 184);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 465);
+            this.Controls.Add(this.groupBoxCategory);
+            this.Controls.Add(this.treeViewCategory);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStripCategory.ResumeLayout(false);
+            this.groupBoxCategory.ResumeLayout(false);
+            this.groupBoxCategory.PerformLayout();
+            this.groupBoxInOrOut.ResumeLayout(false);
+            this.groupBoxInOrOut.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeViewCategory;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripCategory;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemNew;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemModify;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemDelete;
+        private System.Windows.Forms.GroupBox groupBoxCategory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBoxInOrOut;
+        private System.Windows.Forms.RadioButton radioButtonOutcome;
+        private System.Windows.Forms.RadioButton radioButtonIncome;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ trunk/JeanieMoney/Forms/Config/CategoryConfig.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using JeanieMoney.Actions;
+using JeanieMoney.Entities;
+
+namespace JeanieMoney.Forms.Config
+{
+    public partial class CategoryConfig : Form
+    {
+        CategoryAction categoryAction=new CategoryAction();
+
+        List<Category> categoryListAll;
+        public CategoryConfig()
+        {
+            InitializeComponent();
+            categoryListAll = categoryAction.retrieveCategoryList();
+            buildupTreeView(categoryListAll, null);
+        }
+        private void buildupTreeView(List<Category> list, TreeNode parent)
+        {
+            if (parent == null)
+            {
+                foreach (Category category in list)
+                {
+                    if (category.ParentId == String.Empty)
+                    {
+                        TreeNode temp = new TreeNode();
+                        temp.Name=category.Id;
+                        temp.Text=category.Name;
+                        temp.ToolTipText = category.Abbr;
+                        treeViewCategory.Nodes.Add(temp);
+                        buildupTreeView(list, temp);
+                    }
+                }
+            }
+            else
+            {
+                foreach (Category category in list)
+                {
+                    if (category.ParentId == parent.Name)
+                    {
+                        TreeNode temp = parent.Nodes.Add(category.Id, category.Name);
+                        temp.ToolTipText = category.Abbr;
+                        buildupTreeView(list, temp);
+                    }
+                }
+            }
+        }
+
+        private void treeViewCategory_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+                contextMenuStripCategory.Show(e.Location);
+            else
+            {
+                //change parent
+
+            }
+        }
+
+        private void toolStripMenuItemNew_Click(object sender, EventArgs e)
+        {
+            groupBoxCategory.Show();
+        }
+
+        private void treeViewCategory_DragDrop(object sender, DragEventArgs e)
+        {
+            //获得拖放中的节点
+            TreeNode moveNode = (TreeNode)e.Data.GetData("System.Windows.Forms.TreeNode");
+
+            //根据鼠标坐标确定要移动到的目标节点
+            Point pt;
+            TreeNode targeNode;
+            pt = ((TreeView)(sender)).PointToClient(new Point(e.X, e.Y));
+            targeNode = treeViewCategory.GetNodeAt(pt);
+            if (targeNode == null)
+            {
+                TreeNode NewMoveNode = (TreeNode)moveNode.Clone();
+                treeViewCategory.Nodes.Add(NewMoveNode);
+            }
+            else
+            {
+
+           
+                TreeNode NewMoveNode = (TreeNode)moveNode.Clone();
+
+                    targeNode.Nodes.Add( NewMoveNode);
+             
+                //更新当前拖动的节点选择
+                treeViewCategory.SelectedNode = NewMoveNode;
+                //展开目标节点,便于显示拖放效果
+                targeNode.Expand();
+            }
+
+            //移除拖放的节点
+            moveNode.Remove();
+        }
+
+        private void treeViewCategory_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                DoDragDrop(e.Item, DragDropEffects.Move);
+            }
+        }
+
+        private void treeViewCategory_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("System.Windows.Forms.TreeNode"))
+            {
+                e.Effect = DragDropEffects.Move;
+                Point pt;
+                TreeNode targeNode;
+                pt = ((TreeView)(sender)).PointToClient(new Point(e.X, e.Y));
+                targeNode = treeViewCategory.GetNodeAt(pt);
+                if (targeNode != null&&targeNode.no)
+                {
+                    targeNode.Expand();
+                }
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+    }
+}
